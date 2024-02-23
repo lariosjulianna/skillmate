9bdad2fbc27b2290c6726392ad316e599ebc8638
mtansy <115101173+mtansy@users.noreply.github.com>
2024-02-22 17:54:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+import mysql.connector
+import csv
+import tkinter as tk
+from tkinter import simpledialog, messagebox, Tk, Label, Button
 
+# Make a connection to your MySQL server
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="9009-Ocean!",
+    database="FinanceTracker",
+    auth_plugin='mysql_native_password'
+)
