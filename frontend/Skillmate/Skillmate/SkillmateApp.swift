5abd09a4894dc2c9f0e6031e29c32e9cc87ef86b
mtansy <115101173+mtansy@users.noreply.github.com>
2024-02-28 21:44:10 -0800

    uploaded frontend (swift)

---
+++ frontend/Skillmate/Skillmate/SkillmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillmateApp.swift
+//  Skillmate
+//
+//  Created by madi tansy on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SkillmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
