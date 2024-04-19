3130b3ebb699ec68e39c1c41e2b36ac0091b75c0
Carina Chan <carinazchan@gmail.com>
2024-04-18 18:05:30 -0700

    Created Login page, No Account Found page

---
+++ SkillmateLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillmateLoginApp.swift
+//  SkillmateLogin
+//
+//  Created by Carina Chan on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SkillmateLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Login()
+        }
+    }
+}
